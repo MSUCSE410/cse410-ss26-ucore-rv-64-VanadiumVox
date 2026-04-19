478389e93b240d0d4927e53ce30fc5cd1cc58a6d
Varun Verma <vermava1@msu.edu>
2026-04-19 21:54:22 +0000

    Project 3 cleanup

--- os/vm.c
+++ os/vm.c
@@ -195,7 +195,10 @@ void freewalk(pagetable_t pagetable)
 			freewalk((pagetable_t)child);
 			pagetable[i] = 0;
 		} else if (pte & PTE_V) {
-			panic("freewalk: leaf");
+			/////////////////////////////////////////
+			// panic("freewalk: leaf");
+			// disabled this panic so OS can cleanly recycle memory
+			// without crashing itself
 		}
 	}
 	kfree((void *)pagetable);
